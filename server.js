c172642d9922b36a48fc681ee0eda3831061f1aa
Harshit <7harshit20@gmail.com>
2021-12-23 10:59:43 +0530

    prepared to deploy version 1.0

--- server.js
+++ server.js
@@ -10,5 +10,11 @@ app.use(cookieParser());
 app.use('/api/register', require('./route/register'));
 app.use('/api/auth', require('./route/auth'));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}...`));
