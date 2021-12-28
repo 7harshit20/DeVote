7dea4f40518fc819c7d63534874c4f75a2927d47
Harshit <7harshit20@gmail.com>
2021-12-28 14:16:56 +0530

    switched to mumbai testnet

--- client/src/ethereum/devote.js
+++ client/src/ethereum/devote.js
@@ -1,4 +1,7 @@
 const web3 = require('./web3')
 const Devote = require('./build/Devote.json');
 
-module.exports = new web3.eth.Contract(Devote.abi, '0x3EEDFa0d5850a842F32ECf0c00F124e71C245202');
+module.exports = new web3.eth.Contract(Devote.abi, '0x9aC60008c47D8bcA497c6a2241990F35e4FF1e0D');
+
+// Rinkeby: 0x3EEDFa0d5850a842F32ECf0c00F124e71C245202
+// Mumbai: 0x9aC60008c47D8bcA497c6a2241990F35e4FF1e0D
